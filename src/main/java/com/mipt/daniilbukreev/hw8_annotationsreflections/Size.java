fae57b9595d53aeb058680b98fea9b73fc321fd0
danbuk <daniilbukreev7120@mail.ru>
2025-10-27 16:24:13 +0300

    task

---
+++ src/main/java/com/mipt/daniilbukreev/hw8_annotationsreflections/Size.java
@@ -0,0 +1,15 @@
+package com.mipt.daniilbukreev.hw8_annotationsreflections;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Size {
+    int min();
+    int max();
+    String message() default "";
+}
